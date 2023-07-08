5a6d7bf1042a24487a7fc4991d2b9f19ec2bb757
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-07-08 21:31:45 +0530

    implementing discovery server

---
+++ discovery/src/test/java/com/nishath/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishath.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
