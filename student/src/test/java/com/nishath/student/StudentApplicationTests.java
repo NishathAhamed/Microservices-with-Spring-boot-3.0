982df120849e05261c94065324f7a1df7fde67b0
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-07-09 20:52:16 +0530

    adding student microservice

---
+++ student/src/test/java/com/nishath/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishath.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
